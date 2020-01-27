1873fa066b90969c6dfffa7214ab853dd6527597
btoddsubjectdata <btodd@subjectdata.io>
2020-01-27 01:04:08 -0600

    MIN_PROBE_EDGE 5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -926,7 +926,7 @@
 #define NOZZLE_TO_PROBE_OFFSET { -48, -10, -1.525 }
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 5
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 10000
